732ed681ba9f168639edbbc28014745392951a8b
mlgvla <mlgerardvla@gmail.com>
2021-07-09 22:25:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    let userData = {
+        name: name,
+        email: email
+    }
+
+    let userURL = "http://localhost:3000/users"
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    }
+
+    return fetch(userURL, configObj)
+        .then(resp => resp.json())
+        .then(data => {
+            document.body.innerHTML = data.id
+
+        })
+        .catch(error => {
+            document.body.innerHTML = error.message
+        })
+}
